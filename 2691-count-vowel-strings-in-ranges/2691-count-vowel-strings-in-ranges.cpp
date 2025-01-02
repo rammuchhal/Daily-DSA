55d3a497476e9d184ca76f8c3ef7b7b06891c7b0
rammuchhal <123000795+rammuchhal@users.noreply.github.com>
2025-01-02 16:44:59 +0530

    Time: 8 ms (51.42%), Space: 69.4 MB (68.33%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
@@ -2,30 +2,21 @@ class Solution {
 public:
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
         vector<int> ans(queries.size(),0);
-        vector<int> preSumvowel(words.size(),0);
+        vector<int> preSumvowel(words.size()+1,0);
+
+        unordered_set<char> vowel;
+        vowel.insert({'a','e','i','o','u'});
+
         int count=0;
         for(int i=0;i<words.size();i++){
             int endI = words[i].size()-1;
-            cout<<words[i][0]<<" "<<words[i][endI]<<" ";
-            if( (words[i][0]=='a' || words[i][0]=='e' || words[i][0]=='i' || words[i][0]=='o' || words[i][0]=='u') 
-                && (words[i][endI]=='a' || words[i][endI]=='e' || words[i][endI]=='i' || words[i][endI]=='o' || words[i][endI]=='u') ){
-                    count= count+1;   
-                    cout<<count<<" ";
-                }
-                cout<<count<<" ";
-                preSumvowel[i] = count;
-        }
-        for(int i=0;i<preSumvowel.size();i++){
-            cout<<preSumvowel[i]<<" ";
+            if( vowel.find(words[i][0])!=vowel.end() && vowel.find(words[i][endI])!=vowel.end()) count++;   
+                preSumvowel[i+1] = count;
         }
 
         for(int i=0;i<queries.size();i++){
             int start=queries[i][0],end=queries[i][1];
-            if(start==0) ans[i]=preSumvowel[end];
-            else{
-                if(preSumvowel[start]==preSumvowel[start-1]) ans[i]=preSumvowel[end]-preSumvowel[start];
-                else ans[i]=preSumvowel[end]-preSumvowel[start]+1;
-            }
+            ans[i]=preSumvowel[end+1]-preSumvowel[start];
         }
         return ans;
     }
