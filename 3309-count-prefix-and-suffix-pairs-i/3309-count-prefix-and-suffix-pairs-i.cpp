ba71d7b94163961b63fa9e3cf9266a6599075973
rammuchhal <123000795+rammuchhal@users.noreply.github.com>
2025-01-08 13:22:22 +0530

    Time: 7 ms (33.69%), Space: 24.2 MB (22.46%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
@@ -6,11 +6,9 @@ public:
     }
     int countPrefixSuffixPairs(vector<string>& words) {
         int count=0;
-
         for(int i=0;i<words.size()-1;i++){
-            for(int j=i+1;j<words.size();j++){
+            for(int j=i+1;j<words.size();j++)
                 if(isPrefixAndSuffix(words[i],words[j])) count++;
-            }
         }
         return count;
     }
