8688a65e76a5225baf04a27cc41d7b8522fb007a
rammuchhal <123000795+rammuchhal@users.noreply.github.com>
2025-02-22 22:19:52 +0530

    Time: 8 ms (22.54%), Space: 20.7 MB (85.56%) - LeetHub

---
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* recoverFromPreorder(string traversal) {
+        stack<TreeNode*> stack;
+        int index = 0;
+
+        while (index < traversal.size()) {
+            int depth = 0;
+            while (index < traversal.size() && traversal[index] == '-') {
+                depth++;
+                index++;
+            }
+
+            int value = 0;
+            while (index < traversal.size() && isdigit(traversal[index])) {
+                value = value * 10 + (traversal[index] - '0');
+                index++;
+            }
+
+            TreeNode* node = new TreeNode(value);
+
+            while (stack.size() > depth) {
+                stack.pop();
+            }
+
+            if (!stack.empty()) {
+                if (stack.top()->left == nullptr) {
+                    stack.top()->left = node;
+                } else {
+                    stack.top()->right = node;
+                }
+            }
+
+            stack.push(node);
+        }
+
+        while (stack.size() > 1) {
+            stack.pop();
+        }
+
+        return stack.top();
+    }
+};
