07404cb7f8f71329b491244fdde9b8caf03df83e
rammuchhal <123000795+rammuchhal@users.noreply.github.com>
2025-03-05 18:41:25 +0530

    Time: 56 ms (60.8%), Space: 79.2 MB (94.84%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int getmaxProfit(int i,int buy,int tran,vector<int>& prices,int& n,vector<vector<vector<int>>>& dp){
-        if(i==n) return 0;
-        if(tran==0) return 0;
-
-        if(dp[i][buy][tran]!=-1) return dp[i][buy][tran];
-        if(buy){
-            return dp[i][buy][tran]=max(-prices[i]+getmaxProfit(i+1,0,tran,prices,n,dp),getmaxProfit(i+1,1,tran,prices,n,dp));
-        }
-        return dp[i][buy][tran]=max(prices[i]+getmaxProfit(i+1,1,tran-1,prices,n,dp),getmaxProfit(i+1,0,tran,prices,n,dp));
-    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return getmaxProfit(0,1,2,prices,n,dp);
+        vector<vector<int>> after(2,vector<int>(3,0)),curr(2,vector<int>(3,0));
+
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                for(int tran=1;tran<=2;tran++){
+                    if(buy){
+                        curr[1][tran]=max(-prices[i]+after[0][tran],after[1][tran]);
+                    }
+                    else{
+                        curr[0][tran]=max(prices[i]+after[1][tran-1],after[0][tran]);
+                    }
+                }
+            }
+            after=curr;
+        }
+
+        return after[1][2];
     }
 };
