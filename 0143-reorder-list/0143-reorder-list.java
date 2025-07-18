d4fb30d1eb25649dac1a6dfa196a1a154dbbc14a
rammuchhal <123000795+rammuchhal@users.noreply.github.com>
2025-07-18 12:30:21 +0530

    Time: 1 ms (99.61%), Space: 48.1 MB (70.11%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -9,77 +9,38 @@
  * }
  */
 class Solution {
-    public int getLength(ListNode head){
-        ListNode temp=head;
-        int length=0;
-        while(temp!=null){
-            length++;
-            temp=temp.next;
-        }
-        return length;
-    }
-    public ListNode getTailNode(ListNode head){
-        ListNode temp=head;
-        while(temp.next!=null){
-            temp=temp.next;
-        }
-        return temp;
-    }
-    public ListNode pushToStack(ListNode head,int start,int end,Stack<ListNode> stk){
-        for(int i=start;i<end;i++){
-            stk.push(head);
-            ListNode frwd=head.next;
-            head.next=null;
-            head=frwd;
-        }
-        return head;
-    }
-    public ListNode addToLL(Stack<ListNode> stk,ListNode tail){
-        while(!stk.isEmpty()){
-            ListNode node=stk.peek();
-            stk.pop();
-            tail.next=node;
-            tail=node;
-        }
-        return tail;
-    }
     public void reorderList(ListNode head) {
-
         if(head==null || head.next==null) return;
 
-        Stack<ListNode> stk=new Stack<>();
+        ListNode slow=head,fast=head;
 
-        int length=getLength(head);
-
-        head=pushToStack(head,0,length/2,stk);
-
-        ListNode tail=getTailNode(head);
-
-        tail=addToLL(stk,tail);
-
-        head=pushToStack(head,length/2,length,stk);
-        
-        tail=addToLL(stk,tail);
-        
-        head=pushToStack(head,0,length/2,stk);
-
-        while(!stk.isEmpty()){
-            ListNode node=stk.peek();
-            stk.pop();
-            tail.next=node;
-            tail=node;
-            tail.next=head;
-            tail=head;
-            ListNode forw=head.next;
-            head.next=null;
-            head=forw;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
 
-        if(length%2!=0){
-            ListNode frwd=head.next;
-            tail.next=head;
-            head.next=null;
-            head=frwd;
+        ListNode head1=head;
+        ListNode head2=slow.next;
+        slow.next=null;
+
+        ListNode prev=null,curr=head2;
+        while(curr!=null){
+            ListNode frwd=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=frwd;
+        }
+
+        head2=prev;
+
+        while(head1!=null && head2!=null){
+            ListNode frwd1=head1.next;
+            head1.next=head2;
+            head1=frwd1;
+
+            ListNode frwd2=head2.next;
+            head2.next=head1;
+            head2=frwd2;
         }
     }
 }
