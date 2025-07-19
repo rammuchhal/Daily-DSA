2d37bedb2d865911fa141044681131fac399978f
rammuchhal <123000795+rammuchhal@users.noreply.github.com>
2025-07-19 13:43:37 +0530

    Time: 1 ms (95.16%), Space: 43 MB (70.3%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -17,46 +17,24 @@ class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         Queue<TreeNode> q=new LinkedList<>();
         List<List<Integer>> ans=new ArrayList<>();
-        int level=0;
+
         if(root==null) return ans;
-        ans.add(new ArrayList<>());
-        
+
         q.offer(root);
-        int minVal=Integer.MIN_VALUE;
-        TreeNode dummy=new TreeNode(Integer.MIN_VALUE);
-        q.offer(dummy);
-
-        while(!q.isEmpty()){
-            TreeNode front=q.peek();
-            q.poll();
-
-            if(front.val==minVal){
-                if(!q.isEmpty()){
-                    ans.add(new ArrayList<>());
-                    level++;
-                    q.offer(dummy);
-                } 
-            }
-            else{
-                if(front.left!=null) q.offer(front.left);
-                if(front.right!=null) q.offer(front.right);
-                ans.get(level).add(front.val);
-            }
-        }
         
-        // while(!s.isEmpty()){
-        //     if(level==ans.size())
-        //         ans.add(new ArrayList<>());
+        while(!q.isEmpty()){
+            int size=q.size();
+            ArrayList<Integer> temp=new ArrayList<>();
             
-        //     while(!s.isEmpty() && s.peek().val!=minVal){
-        //         ans.get(level).add(s.peek().val);
-        //         s.pop();
-        //     }
-        //     if(!s.isEmpty()) s.pop();
-
-        Collections.reverse(ans);
-        //     level++;
-        // }
+            for(int i=0;i<size;i++){
+                TreeNode node=q.peek();
+                q.poll();
+                temp.add(node.val);
+                if(node.left!=null) q.offer(node.left);
+                if(node.right!=null) q.offer(node.right);
+            }
+            ans.add(0,temp);
+        }
         return ans;
     }
 }
