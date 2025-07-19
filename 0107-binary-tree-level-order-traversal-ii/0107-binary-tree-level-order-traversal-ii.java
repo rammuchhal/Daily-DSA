fdad656f91a28291e9b7f81149c4eb2f77da29f4
rammuchhal <123000795+rammuchhal@users.noreply.github.com>
2025-07-19 13:25:23 +0530

    Time: 1 ms (95.16%), Space: 42.7 MB (97.58%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -19,41 +19,44 @@ class Solution {
         List<List<Integer>> ans=new ArrayList<>();
         int level=0;
         if(root==null) return ans;
-
+        ans.add(new ArrayList<>());
+        
         q.offer(root);
         int minVal=Integer.MIN_VALUE;
         TreeNode dummy=new TreeNode(Integer.MIN_VALUE);
         q.offer(dummy);
-        
-        Stack<TreeNode> s=new Stack<>();
 
         while(!q.isEmpty()){
             TreeNode front=q.peek();
             q.poll();
-            if(!q.isEmpty()) s.push(front);
 
             if(front.val==minVal){
-                if(!q.isEmpty()) q.offer(dummy);
+                if(!q.isEmpty()){
+                    ans.add(new ArrayList<>());
+                    level++;
+                    q.offer(dummy);
+                } 
             }
             else{
                 if(front.left!=null) q.offer(front.left);
                 if(front.right!=null) q.offer(front.right);
+                ans.get(level).add(front.val);
             }
         }
         
-        while(!s.isEmpty()){
-            if(level==ans.size())
-                ans.add(new ArrayList<>());
+        // while(!s.isEmpty()){
+        //     if(level==ans.size())
+        //         ans.add(new ArrayList<>());
             
-            while(!s.isEmpty() && s.peek().val!=minVal){
-                ans.get(level).add(s.peek().val);
-                s.pop();
-            }
-            if(!s.isEmpty()) s.pop();
+        //     while(!s.isEmpty() && s.peek().val!=minVal){
+        //         ans.get(level).add(s.peek().val);
+        //         s.pop();
+        //     }
+        //     if(!s.isEmpty()) s.pop();
 
-            Collections.reverse(ans.get(level));
-            level++;
-        }
+        Collections.reverse(ans);
+        //     level++;
+        // }
         return ans;
     }
 }
